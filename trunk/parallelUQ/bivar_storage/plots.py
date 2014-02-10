c3461fa5b306d42932cf0426ce64f0ae4b93458e
Paul Talbot <taoyiliang@gmail.com>
2014-02-10 15:06:55 -0700

    finished bivariate simple uniform samples

--- trunk/parallelUQ/bivar_storage/plots.py
+++ trunk/parallelUQ/bivar_storage/plots.py
@@ -2,38 +2,42 @@ import matplotlib.pyplot as plt
 import cPickle as pk
 import numpy as np
 
-#case = 'poly'
+case = 'simple'
 #case = 'source'
 #case = '1dsup'
-case = '2d'
+#case = '2d'
 
 douni = True
 #douni = False
 
+#donorm = True
+donorm = False
+
+if donorm:
 ##### NORMALS
-plt.figure()
+  plt.figure()
 
 #get Monte Carlo run
-ctrs=[]
-bins=[]
-for line in file(case+'_MC_normal.csv','r'):
-  if line[:3] in ['Cen','Ave','2nd','Var']:
-    continue
-  ctrs.append(float(line.split(',')[0]))
-  bins.append(float(line.split(',')[1]))
-plt.plot(ctrs,bins,'k-',label='MC')
+  ctrs=[]
+  bins=[]
+  for line in file(case+'_MC_normal.csv','r'):
+    if line[:3] in ['Cen','Ave','2nd','Var']:
+      continue
+    ctrs.append(float(line.split(',')[0]))
+    bins.append(float(line.split(',')[1]))
+  plt.plot(ctrs,bins,'k-',label='MC')
 
-for order in [2,4,8,16,32]:
+  for order in ['2-2']:#,4,8,16,32]:
 #for order in [2]:#,4,8,16]:
-  ct,bn=pk.load(file(case+'_SC'+str(order)+'_normal.pk','r'))
-  plt.plot(ct,bn,label='SC'+str(order))
+    ct,bn=pk.load(file(case+'_SC'+order+'_normal.pk','r'))
+    plt.plot(ct,bn,label='SC'+order)
 
-plt.legend()
-plt.title(case+' Problem, Normal Unc')
-plt.xlabel('Solution Value')
-plt.ylabel('Solution Frequency')
+  plt.legend()
+  plt.title(case+' Problem, Normal Unc')
+  plt.xlabel('Solution Value')
+  plt.ylabel('Solution Frequency')
 #plt.axis([0.5,2.5,0,2.1]) #source
-plt.axis([0.9,1.1,0,30]) #1d
+  plt.axis([0.9,1.1,0,30]) #1d
 #plt.axis([0.6,2,0,2]) #poly
 
 if douni:
@@ -48,18 +52,18 @@ if douni:
       continue
     ctrs.append(float(line.split(',')[0]))
     bins.append(float(line.split(',')[1]))
-  plt.plot(ctrs,bins,'k-.',label='MC')
+  plt.plot(ctrs,bins,'ko',label='MC')
 
-  for order in [2,4,8,16,32]:
+  for order in ['2-2']:#,4,8,16,32]:
   #for order in [2]:#,4,8,16]:
-    ct,bn=pk.load(file(case+'_SC'+str(order)+'_uniform.pk','r'))
-    plt.plot(ct,bn,label='SC'+str(order))
+    ct,bn=pk.load(file(case+'_SC'+order+'_uniform.pk','r'))
+    plt.plot(ct,bn,label='SC'+order)
 
   plt.legend()
-  plt.title(case+' Problem, Uniform Unc')
+  plt.title(case+' Problem, Bivariate Uniform Unc')
   plt.xlabel('Solution Value')
   plt.ylabel('Solution Frequency')
 #plt.axis([0.5,2.5,0,2.1]) #source
-  plt.axis([0.96,1.05,0,100]) #poly
+  #plt.axis([0.96,1.05,0,100]) #poly
 plt.show()
 
