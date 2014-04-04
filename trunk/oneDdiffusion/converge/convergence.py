a6fc2914b41e637ab6f421981eb2bfe27df59e5a
Paul Talbot <taoyiliang@gmail.com>
2014-04-04 14:58:59 -0600

    got q for each case, fixed plots

--- trunk/oneDdiffusion/converge/convergence.py
+++ trunk/oneDdiffusion/converge/convergence.py
@@ -49,12 +49,16 @@ for inFileName in inFiles[1:]:
         runs[h][tuple(spts)][1]=sflx[0]
         runs[h][tuple(spts)][2]=sflx[1]
 
+
 #do g1 errs
 err1={}
 err2={}
 errk={}
 hs=runs.keys()[:]
 hs.sort()
+print 'hs: h, N/s'
+for h in hs:
+  print h,200./h
 ref = hs[0]
 for h in hs[1:]:
   err1[h]=1e-14
@@ -63,8 +67,8 @@ for h in hs[1:]:
     if pt=='k':
       errk[h]=abs(runs[h]['k']-runs[ref]['k'])
     else:
-      err1[h]=max(err1[h],abs(runs[h][pt][1]-runs[ref][pt][1]))
-      err2[h]=max(err2[h],abs(runs[h][pt][2]-runs[ref][pt][2]))
+      err1[h]=max(err1[h],abs(runs[h][pt][1]-runs[ref][pt][1])/runs[ref][pt][1])
+      err2[h]=max(err2[h],abs(runs[h][pt][2]-runs[ref][pt][2])/runs[ref][pt][2])
 
 p1 = zip(np.array(err1.keys()),np.array(err1.values()))
 p2 = zip(np.array(err2.keys()),np.array(err2.values()))
@@ -82,22 +86,29 @@ def addSlopes(x,y):
     avy = 0.5*(y[i]+y[i+1])
     pty = y[i+1]
     slope = (y[i+1]-y[i])/(x[i+1]-x[i])
-    plt.text(avx,pty,'%1.2e' %slope,ha='center')
+    plt.text(avx,pty,'%1.2f' %slope,ha='center')
     plt.plot([avx,avx],[avy,pty],'k--')
+
 
 
 def doPlot(x,y,label):
   plt.figure()
-  #plt.plot(x,y,label=label)
-  plt.loglog(x,y,'-o',label=label)
+  x=np.log(x)
+  y=np.log(y)
+  plt.plot(x,y,'-o',label=label)
+  #plt.loglog(x,y,'-o',label=label)
   plt.title(label)
-  plt.xlabel('h (cm)')
-  plt.ylabel('abs err (k)')
+  plt.xlabel('log h')
+  plt.ylabel('log err')
   addSlopes(x,y)
+  #fit linear
+  z=np.polyfit(x,y,1)
+  f=np.poly1d(z)
+  plt.plot(x,f(x),'k:',label='fit slope: %1.2f' %z[0])
   print '\n',label
   for i in range(len(x)):
     print x[i],y[i]
-  #plt.legend()
+  plt.legend(loc=0)
 
 doPlot(p1[:,0],p1[:,1],'g1 soln')
 doPlot(p2[:,0],p2[:,1],'g2 soln')
