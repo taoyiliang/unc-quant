8727de379ff310224ca29805457a5464fe17f3b9
Paul Talbot <taoyiliang@gmail.com>
2014-05-20 08:35:03 -0600

    added databases of diffusion stochastic convergences

--- trunk/parallelUQ/compare/N3/complot.py
+++ trunk/parallelUQ/compare/N3/complot.py
@@ -9,7 +9,7 @@ HCs = ['HC_h5_iso.moments']
 #       'HC_h3_iso.moments',
 #       'HC_h5_iso.moments']
 
-ref=[0,1.0013454858344246,1.8647257169268627e-04]
+ref=[0,1.0025998595112884,1.7000448118653644e-04]
 
 #addPlot(MC,'MC',ref=ref)
 for h in HCs:
@@ -18,13 +18,14 @@ pltMC(MC,'MC',ref=ref)
 
 xs=np.linspace(2,32000,3)
 ysMC = 1e-2/np.sqrt(xs)
-ysHC1 = 2e-4*xs**(-1)
+ysHC1 = 1e-0*xs**(-3)
 #ysHC2 = 5e-5*xs**(-0.5)
 plt.loglog(xs,ysMC,'k:',label=r'$c\ \eta^{-1/2}$')
-plt.loglog(xs,ysHC1,'k-.',label=r'$c\ \eta^{-1}$')
+plt.loglog(xs,ysHC1,'k-.',label=r'$c\ \eta^{-4}$')
 #plt.loglog(xs,ysHC2,'k:')#,label=r'$C_2\eta^{-1/2}$')
-plt.title(r'Error in $k$; $N$=5, $h\sim$0.018')
+plt.title(r'Error in $k$; $N$=3, $h\sim$0.018')
 plt.xlabel(r'PDE Solves $\eta$')
 plt.ylabel('Rel. Error')
 plt.legend(loc=3)
+plt.axis([1,2e4,1e-8,1e-1])
 plt.show()
