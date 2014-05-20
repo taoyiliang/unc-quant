68c9c2b76842653d8368d933253e0465789041eb
Paul Talbot <taoyiliang@gmail.com>
2014-05-20 17:31:27 -0600

    rolling bacak SparseQuads, I broke it with comments somehow

--- trunk/parallelUQ/SparseQuads.py
+++ trunk/parallelUQ/SparseQuads.py
@@ -2,80 +2,101 @@ import numpy as np
 from itertools import product as allcombos
 
 def BasicSparse(N,L,indexset,quadrule,varlist):
-  #re-type index set as numpy array for convenience
-  indexset=np.array(indexset)
-  #get coefficients
   c=np.array(makeCoeffs(N,indexset))
-  #eliminate index pts with coefficients=zero
+  indexset=np.array(indexset)
   survive=np.nonzero(c!=0)
+  #print 'pre-c',c#indexset
   c=c[survive]
   indexset=indexset[survive]
   print '  ...coefficient set established...'
-  #now make the sparse grid for remaining points
-  SG=[] #holds list (multi-quad pt, wt) -> (array, scalar)
+  #print 'survive',indexset
+  SG=[] #holds list (multi-quad pt, wt)
   for j,cof in enumerate(c):
     idx = indexset[j]
-    #need p(i)+1 quad points for each variable
+    #print '\nindex point:',idx
     m = quadrule(idx)+1
-    #get list of new points, weights for index point i
+    #print '  m',m
+    #print '  c',int(c[j])
     new = tensorGrid(N,m,varlist,idx)
-    #append new points to existing points and weights, includes duplicates
+    #print '  points and weights:'
     for i in range(len(new[0])):
+      #print '    ',new[0][i],' | ',new[1][i]
       SG.append( [new[0][i],new[1][i]] )
-      #multiply weights by coefficients, so I don't have to keep coefficients
       SG[-1][1]*=c[j]
+  #TODO DEBUG check sum of weights
+  #print '\n\n'
   return SG
 
 def makeCoeffs(N,indexset):
-  NI=len(indexset) #cardinality of index set
-  c=np.zeros(NI) #initialize c-values
-  iset=indexset[:] #make a copy so I don't change it accidentally
-  #make possible j vectors
+  NI=len(indexset)
+  c=np.zeros(NI)
+  #set up index set as iset
+  iset=indexset[:]
+  #iset=np.array(indexset)
+  #for i,entry in enumerate(iset):
+  #  iset[i]=np.array(entry)
+  #print 'index set:',iset
+  #set up potential js as jset
   zerone=[0,1]
-  sets=[] #a list of the possible values for each entry in j
+  sets=[]
   for n in range(N):
     sets.append(zerone)
   jset=list(allcombos(*sets))
+  #jset=np.array(jset)
+  #for j,entry in enumerate(jset):
+  #  jset[j]=np.array(entry)
   print '\n\nmaking coeffs...'
-  #check all j to see if i+j in index set; if so, add into c[i]
   for i,ix in enumerate(iset):
+    #print '  i:',i,ix
     ix=np.array(ix)
     for j,jx in enumerate(jset):
       jx=np.array(jx)
-      comb = tuple(jx+ix) #tuple makes it easier to check if i+j in iset
+      #print '    j:',j,jx
+      comb = tuple(jx+ix)
+      #print '  i,j,ix+jx',ix,jx,comb,comb in iset,
       if comb in iset:
+        #print ' adding in',(-1)**sum(jx)
         c[i]+=(-1)**sum(jx)
+      #else: print ' not adding.'
+    #print 'final c[%i]:' %i,c[i]
+    #for j in range(i+1,NI):
+    #  d = indexset[j]-indexset[i]
+    #  print '    d:',d
+    #  if d.all()>=0 and d.all()<=1:
+    #    c[i]+=(-1)**sum(d)
+    #print '  c:',c[i]
+  #print 'c:'
+  #for i,cof in enumerate(c):
+  #  print i,cof
   return c
 
 def tensorGrid(N,m,varlist,idx):
+  #print '\n',idx
   quadptlists=[]
   quadwtlists=[]
-  #get the quadrature points for each variable
   for n in range(N):
-    #quadrature order for variable n
     mn = m[n]
-    #var is the instance of the Variable class
+    #print 'mn:',mn
     var=varlist[varlist.keys()[n]]
     var.setQuadrature(mn)
     quadptlists.append(var.pts)
     quadwtlists.append(var.wts)
-  #now do a tensor product of pts and wts
   quadpts = list(allcombos(*quadptlists))
   quadwts = list(allcombos(*quadwtlists))
-  #just need product of weights for each point, not each weight individually
   for k,wtset in enumerate(quadwts):
     quadwts[k]=np.product(wtset)
+  #for i in range(len(quadpts)):
+  #  print quadpts[i],quadwts[i]
+  #print 'wts,pts',quadwts,quadpts
   return quadpts,quadwts
 
 def removeDuplicates(grid,tol=15):
-  #DANGER: this does round points to 15 decimal places.
-  #This should be worth it for the speed, but maybe not always.
   ptwt={}
   for e,entry in enumerate(grid):
     npt = tuple(np.around(entry[0],decimals=tol))
     if npt in ptwt.keys():
-      ptwt[npt]+=entry[1] #combine weights with existing point
+      ptwt[npt]+=entry[1]
     else:
-      ptwt[npt]=entry[1] #add a new point-weight pair
+      ptwt[npt]=entry[1]
   return ptwt
 
