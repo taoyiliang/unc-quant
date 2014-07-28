03424d4539af70666ca6fd2bf02bc58537aaf548
Paul Talbot <taoyiliang@gmail.com>
2014-07-28 09:30:13 -0600

    TD simple N=10 results

---
+++ trunk/parallelUQ/HDMRdriver.py
@@ -0,0 +1,79 @@
+from matplotlib.pyplot import show
+import time
+import datetime as dt
+import os
+import sys
+from GetPot import GetPot
+import Executor as Exec
+import multiprocessing
+import numpy as np
+
+class Driver(object):
+  def __init__(self,argv):
+    self.starttime=time.time()
+    print '\nStarting HDMR UQ at',dt.datetime.fromtimestamp(self.starttime)
+    self.loadInput(argv)
+    self.loadVars()
+    self.finishUp()
+
+  def loadInput(self,argv):
+    cl = GetPot(argv)
+    print argv
+    if cl.search('-i'):
+      self.unc_inp_file=cl.next('')
+      print 'Selected uncertainty input',self.unc_inp_file,'...'
+      #check desired file exists
+      if not os.path.isfile(self.unc_inp_file):
+        raise IOError('Uncertainty input not found: '+self.unc_inp_file)
+      print '  ...found uncertainty input file.'
+    else:
+      raise IOError('Requires and input file using -i.')
+    self.input_file = GetPot(Filename=self.unc_inp_file)
+
+  def loadVars(self):
+    #make a fake executor, just to make the variables
+    #  PROBLEM: this shouldn't actually run the executor!
+    self.ex_type = self.input_file('Problem/executor','not found')
+    print 'Loading executor',self.ex_type
+    self.ex=Exec.ExecutorFactory(self.ex_type,self.input_file)
+
+  def finishUp(self):
+    elapsed=time.time()-self.starttime
+    print 'Driver run time:',elapsed,'sec'
+    print '\nStarting postprocessing...'
+    makePDF = self.input_file('Backend/makePDF',0)
+    if makePDF:
+      print '...sampling ROM...'
+      numSamples = self.input_file('Backends/PDFsamples',-1)
+      if numSamples==-1:
+        print '...Backends/PDFsamples not found; using 1e4...'
+        numSamples = int(1e4)
+      self.makePDF(numSamples)
+
+    #TODO DEBUG
+    #samp = self.ex.ROM([1.,1.])
+    #print 'sparseU(1,1) =',samp
+    #if 'SC' in self.ex.case:
+    #  self.ex.ROMmoment(1)
+    #  self.ex.ROMmoment(2)
+    #self.ex.ROMpdf(M=1e5)
+    writeStuff=bool(self.input_file('Backend/writeOut',0))
+    if writeStuff:
+      self.ex.writeOut()
+
+    show()
+    print '\nDriver complete.\n'
+
+  def makePDF(self,M):
+    wantprocs = self.input_file('Problem/numprocs',1)
+    numprocs = min(wantprocs,multiprocessing.cpu_count())
+    nBins = self.input_file('Backends/PDF/bins',10)
+    binmin = self.input_file('Backends/PDF/min',-10)
+    binmax = self.input_file('Backends/PDF/max',10)
+    bins=np.linspace(binmin,binmax,nBins+1)
+    self.ex.makePDF(numprocs,M,bins)
+
+
+if __name__=='__main__':
+  #print sys.argv,type(sys.argv)
+  drv = Driver(sys.argv)
