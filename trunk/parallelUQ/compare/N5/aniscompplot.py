2d382f415f4bddb924f2ebbf27b8c1910e9489ba
Paul Talbot <taoyiliang@gmail.com>
2014-08-19 09:13:48 -0600

    storing some working edits

--- trunk/parallelUQ/compare/N5/aniscompplot.py
+++ trunk/parallelUQ/compare/N5/aniscompplot.py
@@ -3,6 +3,8 @@ from plotMC import addPlot as pltMC
 import matplotlib.pyplot as plt
 import numpy as np
 
+r=2
+
 MC = 'MC_h5_iso.samples'
 iso = 'HC_h5_N5_iso.moments'
 plots= [
@@ -13,10 +15,11 @@ plots= [
 
 ref=[0,1.0013454858344246,1.8647257169268627e-04]
 
-addPlot(iso,"iso",ref=ref)
-for p in plots:
-  addPlot(p,p.split('_')[2].split('.')[0],ref=ref)
-pltMC(MC,'MC',ref=ref)
+addPlot(iso,"iso",ref=ref,r=r)
+for i,p in enumerate(plots):
+  if i==1:
+    pltMC(MC,'MC',ref=ref,r=r)
+  addPlot(p,p.split('_')[2].split('.')[0],ref=ref,r=r)
 
 xs=np.linspace(2,32000,3)
 ysMC = 1e-2/np.sqrt(xs)
@@ -25,9 +28,9 @@ ysHC1 = 1e-0*xs**(-3)
 #plt.loglog(xs,ysMC,'k:',label=r'$c\ \eta^{-1/2}$')
 #plt.loglog(xs,ysHC1,'k-.',label=r'$c\ \eta^{-4}$')
 #plt.loglog(xs,ysHC2,'k:')#,label=r'$C_2\eta^{-1/2}$')
-plt.title(r'Error in $k$; $N$=5, $h\sim$0.018')
+plt.title(r'Error in var($k$); $N$=5, $h\sim$0.018')
 plt.xlabel(r'PDE Solves $\eta$')
 plt.ylabel('Rel. Error')
 plt.legend(loc=3)
-plt.axis([1,1e4,1e-9,1e-2])
+plt.axis([1,1e4,1e-7,1e-0])
 plt.show()
