f2fd93e95976432549a7cce26fbc793c16e79b0e
Paul Talbot <taoyiliang@gmail.com>
2014-08-27 09:06:19 -0600

    updated some version edits

--- trunk/simple/simple.py
+++ trunk/simple/simple.py
@@ -15,20 +15,32 @@ def f(x,y):
 def h(v):
   return np.exp(-np.sum(v))
 
-cl = GetPot(sys.argv)
-if cl.search('-i'):
-  inpFileName = ''
-  inpFileName = cl.next(inpFileName)
-  input_file=GetPot(Filename=inpFileName)
-else: raise IOError('Requires an input file using -i.')
+def h5(v):
+  while len(v)<5:
+    v.append(3.5)
+  return np.exp(-np.sum(v))
 
-x = input_file('Problem/x',1.0)
-y = input_file('Problem/y',1.0)
-outFileName = input_file('Output/file','test.out')
+def h10(v):
+  while len(v)<10:
+    v.append(3.5)
+  for i,val in enumerate(v):
+    v[i]=val*0.1
+  return np.exp(-np.sum(v))
+
+#cl = GetPot(sys.argv)
+#if cl.search('-i'):
+#  inpFileName = ''
+#  inpFileName = cl.next(inpFileName)
+#  input_file=GetPot(Filename=inpFileName)
+#else: raise IOError('Requires an input file using -i.')
+
+#x = input_file('Problem/x',1.0)
+#y = input_file('Problem/y',1.0)
+#outFileName = input_file('Output/file','test.out')
 
 #result = g(x)
-result = f(x,y)
+#result = f(x,y)
 
-writeFile = file(outFileName,'w')
-writeFile.writelines('res,'+str(result)+'\n')
-writeFile.close()
+#writeFile = file(outFileName,'w')
+#writeFile.writelines('res,'+str(result)+'\n')
+#writeFile.close()
