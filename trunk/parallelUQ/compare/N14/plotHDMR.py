09143127c0d56ff1ca95d813554e10537900ca07
Paul Talbot <taoyiliang@gmail.com>
2014-10-20 10:59:41 -0600

    results for diffusion N=5, N=14, 14 still ongoing for HDMR3

---
+++ trunk/parallelUQ/compare/N14/plotHDMR.py
@@ -0,0 +1,64 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def addPlot(title,lbl,ref=None,r=1,slnfig=None):
+  if r>1:
+    print 'HDMR cannot do r>1, tried to do r =',r
+    return
+  inFile = file(title,'r')
+
+  entries=[]
+  for line in inFile:
+    if line=='\n' or line.startswith('Runs') or line.startswith('N,')\
+        or line.startswith('#'):
+      continue
+    entries.append([])
+    vals=line.strip().split(',')
+    entries[-1].append(int(vals[0]))
+    entries[-1].append(int(vals[1]))
+    entries[-1].append(float(vals[2]))
+
+  entries=np.array(entries)
+  entries=entries[entries[:,0].argsort()]
+
+  if slnfig:
+    errfig = plt.gcf()
+    entr=entries.copy()
+    #entr[:,1]=(1.-entr[:,1])/entr[:,1]
+    #entr[:,2]=(1.-entr[:,2])/entr[:,2]
+    entr=zip(*entr)
+    plt.figure(slnfig.number)
+    plt.plot(entr[0],entr[2],'-x',label=lbl)
+    plt.figure(errfig.number)
+
+  errs=np.zeros([len(entries),3])
+  if ref==None:
+    errs[:,0] = entries[:-1,0]
+    #errs[:,1] = abs(entries[:-1,1]-entries[-1,1])/entries[-1,1]
+    errs[:,1] = abs(entries[:-1,2]-entries[-1,2])/entries[-1,2]
+  else:
+    errs[:,0] = entries[:,0]
+    #errs[:,1] = abs(entries[:,1]-ref[1])/ref[1]
+    errs[:,1] = abs(entries[:,2]-ref[1])/ref[1]
+
+  #for e in errs:
+  #  print e
+  errs=errs[errs[:,0].argsort()]
+#  print '\n\n'
+  #for e in errs:
+  #  print e
+  errs=zip(*errs)
+  plt.loglog(errs[0],errs[r],'-o',label=lbl)
+
+
+
+
+if __name__=='__main__':
+  title = 'HC_h3_iso.moments'
+  r=2
+  addPlot(title,'',r=r)
+  plt.xlabel('Number of Solves')
+  plt.ylabel('Error')
+  plt.title('h1, Moment %i' %r)
+  plt.show()
+
